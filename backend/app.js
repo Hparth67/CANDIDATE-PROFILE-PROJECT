042c8cff3595e92cffef9522146af2f7a1f7a8a3
Parth Hande <hparth67@gmail.com>
2025-09-05 00:54:33 +0530

    localhost port updated

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import {sequelize} from './models/index.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL || '*'
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 
 function App() {
